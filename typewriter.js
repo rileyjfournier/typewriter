5dfc66edd683f79076cd84c8285b8261396ffc86
Riley Fournier <rileyjfournier@gmail.com>
2021-02-09 20:43:28 +0000

    delaying each character separately

---
+++ typewriter.js
@@ -0,0 +1,7 @@
+const sentence = "hello there from lighthouse labs";
+
+for (let i = 0; i < sentence.length; i++) {
+  let x = 50;
+  setTimeout(() => {
+    process.stdout.write(sentence[i])}, x*i)
+  }
